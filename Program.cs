4fc9aed1f9c8f62cc7984871014fe2f7dfb4bd4c
Lassesen <Ken@Lassesen.com>
2024-11-15 19:45:20 -0800

    Moved from H

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PhotoScan;
+
+Console.WriteLine("Hello, World!");
+string[] supportedExtensions = { ".mp4", ".avi", ".mkv", ".mov", ".wmv", ".flv", ".webm" };
+foreach (var folder in Directory.GetDirectories(@"F:\"))
+{
+    
+    try
+    {
+        string[] photoFiles = Directory.GetFiles(folder, "*.*", SearchOption.AllDirectories)
+            .Where(file => supportedExtensions.Contains(Path.GetExtension(file).ToLower()))
+            .ToArray();
+        var cnt=VideoData.ScanFiles(photoFiles);
+        Console.WriteLine($"{folder} {cnt}");
+    }
+    catch(Exception exc)
+    {
+        Console.WriteLine(exc.Message);
+    }
+}
+Console.WriteLine("Done!");
---
+++ VideoData.cs
@@ -0,0 +1,39 @@
+﻿using Shell32;
+using System.IO;
+using MetadataExtractor;
+
+namespace PhotoScan
+{
+    internal class VideoData
+    {
+        public static Dictionary<string, string> photos = new Dictionary<string, string>();
+        public static int ScanFiles(string[] photoFiles)
+        {
+            int cnt = 0;
+            string outFolder = "W:\\Video";
+            foreach (string photoFile in photoFiles)
+            {
+                var x= MetadataExtractor.ImageMetadataReader.ReadMetadata(photoFile);
+                string outfolder = "";
+                var fileInfo = new FileInfo(photoFile);
+
+                //outfolder = Path.Combine(outFolder, $"{fileInfo.Extension}\\{createTime.Year}\\{createTime.Month}\\{createTime.Day}");
+
+                var folder = new DirectoryInfo(outfolder);
+                if (!folder.Exists)
+                {
+                    folder.Create();
+                }
+                var outfile = Path.Combine(outfolder, fileInfo.Name);
+                if (!File.Exists(outfile))
+                {
+                    File.Copy(photoFile, outfile);
+                    cnt++;
+                }
+
+            }
+            return cnt;
+        }
+
+    }
+}
